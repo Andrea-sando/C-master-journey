99a9490e471fd050ccd32564da35cdfae5ba7e40
Andrea-sando <a15957136@gmail.com>
2026-03-16 21:21:59 +0100

    Update sum_even_numbers.c
    This file sums all even numbers from 0 up to the number entered by the user.

--- 01-basic-logic/sum_even_numbers.c
+++ 01-basic-logic/sum_even_numbers.c
@@ -1,1 +1,23 @@
 
+#include <stdio.h>
+
+int main() {
+    int i, number, sum = 0;
+
+    // Ask the user to enter a number
+    printf("Tell me the number:\n");
+    scanf("%d", &number);
+
+    // Loop from 0 up to the entered number
+    for(i = 0; i <= number; i++){
+        // If i is even, add it to sum
+        if(i % 2 == 0){
+            sum = sum + i;
+        }
+    }
+
+    // Print the result
+    printf("The sum of even numbers from 0 to %d is: %d\n", number, sum);
+
+    return 0;
+}
